e8f21fab5493ff7132e4517a8e00c59148d12f4c
yeongun130 <yeongunj625@gmail.com>
2023-03-02 17:02:53 +0900

    xml 가져오기, 익명함수 실습

--- app/src/main/java/com/example/myapplication/Listener.kt
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import com.example.myapplication.databinding.ActivityListenerBinding
 
@@ -18,5 +20,28 @@ class Listener : AppCompatActivity() {
         // val textView : TextView = findViewById(R.id.txt_hello)
         // 2> ViewBinding
         // binding.txtHello.
+
+        with(binding) {
+            // 익명 함수
+            // 1 -> 람다 방식
+            txtHello.setOnClickListener {
+                Log.d("Click","Click!!")
+            }
+
+            // 2 -> 익명 함수 방식
+            txtHello.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    Log.d("Click","Click!!")
+                }
+            })
+
+            // 3 -> 이름이 필요한 경우
+            val click = object : View.OnClickListener{
+                override fun onClick(p0: View?) {
+                }
+            }
+
+            txtHello.setOnClickListener(click)
+        }
     }
 }
